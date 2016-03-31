11bb0068b73b383dd6c6939f099630f4f250ad7e
mlcaamano <millo-x-100pre@hotmail.com>
2016-03-31 20:38:39 -0300

    Funciona solo con papel

--- app.js
+++ app.js
@@ -1,10 +1,55 @@
 var angapp=angular.module("MiApp",[]);
 angapp.controller('ContPPT2', function($scope){
 
-$scope.empatadas="10";
+$scope.empatadas="0";
+$scope.ganadas="0";
+$scope.perdidas="0";
 
+// $scope.eleccionMaquina="papel";
 
+$scope.comenzar=function(){
 
+		 	numeroSecreto =Math.floor( Math.random()*3)+1;
+		//alert(numeroSecreto);
+		switch(numeroSecreto)
+		{
+			case 1:
+				$scope.eleccionMaquina="piedra";
+				break;
+			case 2:
+				$scope.eleccionMaquina="papel";
+				break;
+			case 3:
+				$scope.eleccionMaquina="tijera";
+				break;
+
+		}
+}
+
+$scope.papel=function(){
+
+	// alert("hola");
+	$scope.comenzar();
+	alert("la maquina selecciono: "+$scope.eleccionMaquina);
+	$scope.eleccionHumano="papel";
+	if($scope.eleccionHumano==$scope.eleccionMaquina)
+	{
+		alert("empate.");
+		$scope.empatadas++;
+
+	}
+	else if($scope.eleccionMaquina=="piedra")
+	{
+		alert("vos ganastes.");
+		$scope.ganadas++;
+	}
+	else
+	{
+		alert("ganó la Maquina.");
+		$scope.perdidas++;
+	}
+ }
+// $scope.papel();
 
 
 
